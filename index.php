e851375f1184877bf9c5141fedc77fda4595ab88
Alexandre Garcia Perez <alexandresilvinogarciaperez@student.vinci.be>
2021-04-20 22:49:52 +0200

    index modifier switch

--- index.php
+++ index.php
@@ -8,11 +8,36 @@
 
 	$db=Db::getInstance();
 
-	include(VIEWS_PATH.'header.php'); 
-	
-	require_once('controllers/HomeController.php');	
-	$controller = new HomeController($db);
-	$controller->run();
+	require_once(VIEWS_PATH.'header.php'); 
 
-	include(VIEWS_PATH.'footer.php');
+	if (empty($_GET['action'])) {
+        $_GET['action'] = 'home';
+    }
+	
+	switch ($_GET['action']) {
+        case 'profil':  # action=Profil
+            require_once(CONTROLLERS_PATH.'ProfilController.php');
+            $controller = new ProfilController($db);
+            break;
+		case 'login':  # action=Login
+            require_once(CONTROLLERS_PATH.'LoginController.php');
+            $controller = new LoginController($db);
+            break;
+		case 'registration':  # action=registration
+            require_once(CONTROLLERS_PATH.'RegistrationController.php');
+            $controller = new RegistrationController($db);
+            break;
+		case 'timelineidea':  # action=timelineidea
+            require_once(CONTROLLERS_PATH.'TimeLineIdeaController.php');
+            $controller = new TimeLineIdeaController($db);
+            break;
+
+        default:        # dans tous les autres cas l'action=home
+            require_once(CONTROLLERS_PATH.'HomeController.php');
+            $controller = new HomeController($db);
+            break;
+    }
+		$controller->run();
+
+	require_once(VIEWS_PATH.'footer.php');
 ?>
