873b36924a1ffff2e87d172dfd3f4665b257cbd1
Alexandre Garcia Perez <alexandresilvinogarciaperez@student.vinci.be>
2021-04-20 22:50:37 +0200

    header modifier

--- views/header.php
+++ views/header.php
@@ -3,5 +3,22 @@
 	<head>
 		<meta charset="utf-8">
 		<title>studentvote</title>
-	</head>
+		</head>
 	<body>
+	<header>
+    <h1>
+        titre principale 
+    </h1>
+    <p class="sous-titre">
+        <strong>HE Vinci Paul Lambin</strong> : bloc 1 en informatique
+    </p>
+    <nav>
+        <ul>
+			<li><a href="index.php?action=home">the hom</a></li>
+            <li><a href="index.php?action=profil">votre profil</a></li>
+            <li><a href="index.php?action=login"> le logine </a></li>
+            <li><a href="index.php?action=registration"> le registration </a></li>
+            <li><a href="index.php?action=timelineidea"> la timelineidea </a></li>
+        </ul>
+    </nav>
+</header>
