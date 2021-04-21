edba8f47b0d8f4c5bb41e86ed68874ad953dd4d5
Alexandre Garcia Perez <alexandresilvinogarciaperez@student.vinci.be>
2021-04-21 10:32:04 +0200

    header and registration modifier

--- views/header.php
+++ views/header.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title>studentvote</title>
-        <link rel="stylesheet" type="text/css" href="<?php echo VIEWS_PATH ?>css/teste.css" media="all">
+        <link rel="stylesheet" type="text/css" href="<?php echo VIEWS_PATH ?>css/Base.css" media="all">
 		</head>
 	<body>
 	<header>
@@ -17,7 +17,7 @@
         <ul>
 			<li><a href="index.php?action=home">the home </a></li>
             <li><a href="index.php?action=timelineidea"> la idealistadmin </a></li>
-            <li><a href="index.php?action=login"> le logine </a></li>
+            <li><a href="index.php?action=login"> le login </a></li>
             <li><a href="index.php?action=memberlistadmin"> votre liste de membre </a></li> 
             <li><a href="index.php?action=postcomments"> les commentaire </a></li> 
             <li><a href="index.php?action=profil"> votre profil</a></li>    
--- views/registration.php
+++ views/registration.php
@@ -1,1 +1,10 @@
 <p><?php echo $notification ?></p>
+
+
+<from id="register" class="input-group">
+    Your User Name <br> <input type="texte" class="input-flield" placeholder="User Id" required><br>
+    Your Email <br> <input type="email" class="input-flield" placeholder="Email Id" required><br>
+    Your Password <br> <input type="texte" class="input-flield" placeholder="Enter Password" required><br>
+    <input type="checkbox" class="chech-box"><span> i agree to the terms & conditions</span><br>
+    <button type="submit" class="submit-btn">Register</button>
+</from>
