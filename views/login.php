0fa18d758f1d7f2b57b5981c0e074845acc80bd5
Cédric Niyikiza <cedric.niyikiza@student.vinci.be>
2021-04-23 12:03:48 +0200

    Traduction de la vue et modification de quelques éléments du formulaire

--- views/login.php
+++ views/login.php
@@ -1,16 +1,15 @@
 <p><?php echo $notification ?></p>
 <form action="action_page.php" method="post">    
     <img src="views/images/Default_Avatar.png" alt="Avatar">
-    <div class="container">
-        <label for="uname"><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="uname" required></br>
+    <div id="form_log_in">
+        <label for="pseudo"><b>Pseudo</b></label> </br>
+        <input type="text" placeholder="Pseudo" name="pseudo" required></br>
 
-        <label for="psw"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="psw" required></br>
+        <label for="password"><b>Mot de passe</b></label> </br>
+        <input type="password" placeholder="Entrez votre mot de passe" name="password" required></br>
 
         <button type="submit">Login</button>
-
-        <p>Si vous n'êtes pas membre veuillez vous enregistrez sur votre base de données.</p>
     </div>
 </form>
+<p>Si vous n'êtes pas membre veuillez vous enregistrez sur votre base de données.</p>
 <a href="index.php?action=registration"><button>Créer un compte</button></a>
--- views/registration.php
+++ views/registration.php
@@ -2,9 +2,13 @@
 
 
 <form id="register" method="POST">
-    Pseudo <br> <input type="texte"placeholder="Pseudo" required><br>
-    Email <br> <input type="email"placeholder="email@adresse.com" required><br>
-    Your Password <br> <input type="password" placeholder="Enter Password" required><br>
-        <a class="button" ><a href="index.php?action=registration">Register</a></a>
-        <a class="button" ><a href="index.php?action=login">login</a></a>
+    Entrez un pseudo <br> <input type="texte"placeholder="Pseudo" required><br>
+    Entrez un email valide <br> <input type="email"placeholder="email@adresse.com" required><br>
+    Entrez un mot de passe <br> <input type="password" placeholder="Enter Password" required><br>
+    </br>
+    <button type="submit">Enregistrez-vous</button>
 </form>
+
+
+<p>Si vous êtes déjà membre veuillez vous connectez.</p>
+<a href="index.php?action=login"><button>Connectez-vous</button></a>
