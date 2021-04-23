8d88a0ef6869c4219b3595d8b1e95fe978d06dab
Cédric Niyikiza <cedric.niyikiza@student.vinci.be>
2021-04-23 11:39:30 +0200

    Modification du formulaire de registration

--- views/registration.php
+++ views/registration.php
@@ -1,11 +1,10 @@
 <p><?php echo $notification ?></p>
 
 
-<from id="register" class="input-group">
-    Your User Name <br> <input type="texte" class="input-flield" placeholder="User Id" required><br>
-    Your Email <br> <input type="email" class="input-flield" placeholder="Email Id" required><br>
-    Your Password <br> <input type="texte" class="input-flield" placeholder="Enter Password" required><br>
-    <input type="checkbox" class="chech-box"><span> i agree to the terms & conditions</span><br>
+<form id="register" method="POST">
+    Pseudo <br> <input type="texte"placeholder="Pseudo" required><br>
+    Email <br> <input type="email"placeholder="email@adresse.com" required><br>
+    Your Password <br> <input type="password" placeholder="Enter Password" required><br>
         <a class="button" ><a href="index.php?action=registration">Register</a></a>
         <a class="button" ><a href="index.php?action=login">login</a></a>
-</from>
+</form>
