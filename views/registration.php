1236960697b9c9d13390c3a1552fcfdf9a9507b8
Alexandre Garcia Perez <alexandresilvinogarciaperez@student.vinci.be>
2021-04-23 12:08:12 +0200

    modification du register

--- views/registration.php
+++ views/registration.php
@@ -1,11 +1,48 @@
-<p><?php echo $notification ?></p>
 
+<h2> Inscription </h2>
+<br /><br />
+<from  class="input-group" method ="POST" action="">
+    <table>
+        <tr>
+            <td>
+                <label for="pseudo">Your User Name :</label>
+            </td>
+            <td>
+                <input type="texte" class="input-flield" placeholder=" your pseudo " id="pseudo" name="pseudo">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label for="email">Your Email Name :</label>
+            </td>
+            <td>
+                <input type="texte" class="input-flield" placeholder=" your email " id="email" name="email">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label for="password">Your Password :</label>
+            </td>
+            <td>
+                <input type="texte" class="input-flield" placeholder=" your password " id="password" name="password">
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <input type="checkbox"  class="chech-box"><span> i agree to the terms & conditions</span>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <input type="submit"  value="Je m'iscris" />
+            </td>
+        </tr>
 
-<from id="register" class="input-group">
-    Your User Name <br> <input type="texte" class="input-flield" placeholder="User Id" required><br>
-    Your Email <br> <input type="email" class="input-flield" placeholder="Email Id" required><br>
-    Your Password <br> <input type="texte" class="input-flield" placeholder="Enter Password" required><br>
-    <input type="checkbox" class="chech-box"><span> i agree to the terms & conditions</span><br>
-        <a class="button" ><a href="index.php?action=registration">Register</a></a>
+    </table>
+    <br />
+    <br>
+        <a type="submit" class="button" ><a href="index.php?action=registration">Register</a></a>
         <a class="button" ><a href="index.php?action=login">login</a></a>
 </from>
