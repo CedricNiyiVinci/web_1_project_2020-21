a841f116175dd1d1f3a0df55979b76ae7d6acf0f
Cédric Niyikiza <cedric.niyikiza@student.vinci.be>
2021-04-23 12:45:30 +0200

    Modif légère dans le registration.php

--- views/registration.php
+++ views/registration.php
@@ -1,42 +1,35 @@
 
 <h2> Inscription </h2>
-<br /><br />
-<form   method ="POST" action="">
+<br/>
+<form id="register" method ="POST" action="">
     <table>
         <tr>
             <td>
-                <label for="pseudo">Your User Name :</label>
+                <label for="pseudo">Entrez un pseudo :</label>
             </td>
             <td>
-                <input type="texte"  placeholder=" your pseudo " id="pseudo" name="pseudo">
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <label for="email">Your Email Name :</label>
-            </td>
-            <td>
-                <input type="texte"  placeholder=" your email " id="email" name="email">
+                <input type="texte"  placeholder="Pseudo" name="pseudo">
             </td>
         </tr>
         <tr>
             <td>
-                <label for="password">Your Password :</label>
+                <label for="email">Entrez un email valide :</label>
             </td>
             <td>
-                <input type="texte"  placeholder=" your password " id="password" name="password">
-            </td>
-        </tr>
-        <tr>
-            <td></td>
-            <td>
-                <input type="checkbox" ><span> i agree to the terms & conditions</span>
+                <input type="texte"  placeholder="email@adresse.com" name="email">
             </td>
         </tr>
         <tr>
-            <td></td>
             <td>
-                <input type="submit"  value="Je m'iscris" />
+                <label for="password">Entrez un mot de passe :</label>
+            </td>
+            <td>
+                <input type="password"  placeholder="Entrez un mot de passe" name="password">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="submit"  value="Je m'inscrit" />
             </td>
         </tr>
     </table>
