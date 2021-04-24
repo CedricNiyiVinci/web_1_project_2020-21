1f8547c5e52c5535f7dd671c730e289ebbbbf842
Alexandre Garcia Perez <alexandresilvinogarciaperez@student.vinci.be>
2021-04-24 12:45:42 +0200

    leger bug corriger

--- views/registration.php
+++ views/registration.php
@@ -1,39 +1,40 @@
 
 <h2> Inscription </h2>
-<br/>
-<form id="register" method ="POST" action="">
+<br /><br />
+<form   method ="POST" action="">
     <table>
         <tr>
             <td>
-                <label for="pseudo">Entrez un pseudo :</label>
+                <label for="pseudo"> votre pseudo :</label>
             </td>
             <td>
-                <input type="texte"  placeholder="Pseudo" name="pseudo">
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <label for="email">Entrez un email valide :</label>
-            </td>
-            <td>
-                <input type="texte"  placeholder="email@adresse.com" name="email">
+                <input type="texte"  placeholder=" your pseudo " id="pseudo" name="pseudo">
             </td>
         </tr>
         <tr>
             <td>
-                <label for="password">Entrez un mot de passe :</label>
+                <label for="email"> votre Email :</label>
             </td>
             <td>
-                <input type="password"  placeholder="Entrez un mot de passe" name="password">
+                <input type="texte"  placeholder=" your email " id="email" name="email">
             </td>
         </tr>
         <tr>
             <td>
-                <input type="submit"  value="Je m'inscrit" />
+                <label for="password"> ton Mots De Passe :</label>
+            </td>
+            <td>
+                <input type="texte"  placeholder=" your password " id="password" name="password">
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <input type="submit"  value="Je m'iscris" />
             </td>
         </tr>
     </table>
 </form>
 <p>Si vous êtes déjà membre veuillez vous connectez.</p>
-<span class ="Mybutton"><a href="index.php?action=login">Connectez-vous</a></span>
+<span class="MyButton"><a href="index.php?action=login">Connectez-vous</a>
 
