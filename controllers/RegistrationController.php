cc0f5f3122999eec06a22e3cf6826500219615d8
Cédric Niyikiza <cedric.niyikiza@student.vinci.be>
2021-04-27 11:01:30 +0200

    Ajout de redirection dans le cas de la page registration

--- controllers/RegistrationController.php
+++ controllers/RegistrationController.php
@@ -8,6 +8,13 @@ class RegistrationController {
 	}
 	
 	public function run(){	
+
+		# Si un distrait écrit ?action=registration en étant déjà authentifié
+		if (!empty($_SESSION['authentifie'])) {
+			header("Location: index.php?action=profile"); # redirection HTTP vers l'action login
+			die(); 
+		}	
+
 		$notification = "Page d'inscription";
 
 		require_once(VIEWS_PATH.'registration.php');
--- views/registration.php
+++ views/registration.php
@@ -1,14 +1,14 @@
 
 <h2> Inscription </h2>
 <br /><br />
-<form   method ="POST" action="">
+<form action="index.php?action=register" method ="POST" >
     <table>
         <tr>
             <td>
                 <label for="pseudo"> votre pseudo :</label>
             </td>
             <td>
-                <input type="texte"  placeholder=" your pseudo " id="pseudo" name="pseudo">
+                <input type="texte"  placeholder=" your pseudo " id="pseudo" name="pseudo" required>
             </td>
         </tr>
         <tr>
@@ -16,7 +16,7 @@
                 <label for="email"> votre Email :</label>
             </td>
             <td>
-                <input type="texte"  placeholder=" your email " id="email" name="email">
+                <input type="email"  placeholder=" your email " id="email" name="email" required>
             </td>
         </tr>
         <tr>
@@ -24,7 +24,7 @@
                 <label for="email"> confirmation Email :</label>
             </td>
             <td>
-                <input type="texte"  placeholder="confirmation Email" id="confirmation_email" name="confirmation_email">
+                <input type="texte"  placeholder="confirmation Email" id="confirmation_email" name="confirmation_email" required>
             </td>
         </tr>
         <tr>
@@ -32,7 +32,7 @@
                 <label for="password"> ton Mots De Passe :</label>
             </td>
             <td>
-                <input type="texte"  placeholder=" your password " id="password" name="password">
+                <input type="texte"  placeholder=" your password " id="password" name="password" required>
             </td>
         </tr>
         <tr>
@@ -40,17 +40,17 @@
                 <label for="password"> confirmation du mots de passe :</label>
             </td>
             <td>
-                <input type="texte"  placeholder="confirmation_password" id="confirmation_password" name="confirmation_password">
+                <input type="texte"  placeholder="confirmation_password" id="confirmation_password" name="confirmation_password" required>
             </td>
         </tr>
         <tr>
             <td></td>
             <td>
-                <input type="submit"  value="Je m'inscris" />
+                <input type="submit" name="form_registration" value="Je m'inscris" />
             </td>
         </tr>
     </table>
-</form>
+</form >
 <p>Si vous êtes déjà membre veuillez vous connectez.</p>
 <span class="MyButton"><a href="index.php?action=login">Connectez-vous</a>
 
