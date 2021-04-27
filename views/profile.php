a5f974403505a8774c76952403cf2b871c3cb258
Cédric Niyikiza <cedric.niyikiza@student.vinci.be>
2021-04-27 14:33:28 +0200

    Modifications minimes

--- views/profile.php
+++ views/profile.php
@@ -3,7 +3,7 @@
 	<p>Bienvenue <strong><?php echo $_SESSION['login']?></strong><?php echo $notification; ?> </p>
 	<p><a href="index.php?action=logout">Se déconnecter</a></p>
 	<h2>Mes idées</h2>
-	<h2>Mes Commentaire</h2>
+	<h2>Mes Commentaires</h2>
 	<h2>Mes idées</h2>
 
 	
